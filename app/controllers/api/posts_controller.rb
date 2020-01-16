c3af030ab3a05e87094c01bfedf25865de9c49b0
ThundaHorse <abeeeeekim@gmail.com>
2020-01-16 12:18:47 -0500

    initial post model and association to users

---
+++ app/controllers/api/posts_controller.rb
@@ -0,0 +1,47 @@
+class Api::PostsController < ApplicationController
+
+  def index
+    @posts = Post.all 
+    render 'index.json.jbuilder'
+  end 
+
+  def show 
+    @post = Post.find(params[:id])
+    render 'show.json.jbuilder'
+  end 
+
+  def create 
+    @post = Post.new(
+      title: params[:title],
+      body: params[:body],
+      user_id: params[:user_id]
+    )
+    
+    if @post.save 
+      render json: { message: "Post created successfully" }, status: 200
+    else 
+      render json: { message: @post.errors.full_messages }
+    end 
+  end 
+
+  def update 
+    @post = Post.find(params[:id])
+
+    @post.title = params[:title] || @post.title 
+    @post.body = params[:body] || @post.body
+    @post.user_id = params[:user_id] || @post.user_id
+
+    if @post.save 
+      render json: { message: "Updated successfully" }, status: 200 
+    else 
+      render json: { errors: @post.errors.full_messages }
+    end 
+
+    def destroy 
+      @post = Post.find(params[:id])
+      @post.delete 
+      render json: { message: "Deleted successfully" }
+    end 
+  end 
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_16_160356) do
+ActiveRecord::Schema.define(version: 2020_01_16_170901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.integer "user_id"
+    t.string "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
