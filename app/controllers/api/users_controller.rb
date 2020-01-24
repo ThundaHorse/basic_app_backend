cb9c2e6f7b839b63b34d8cfe64d22604a60751d0
ThundaHorse <abeeeeekim@gmail.com>
2020-01-24 12:27:30 -0500

    added user image

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -17,6 +17,7 @@ class Api::UsersController < ApplicationController
     @user.first_name = params[:first_name] || @user.first_name
     @user.last_name = params[:last_name] || @user.last_name 
     @user.email = params[:email] || @user.email
+    @user.image = params[:image] || @user.image
     @user.password = params[:password] || @user.password
     @user.password_confirmation = params[:password_confirmation] || @user.password_confirmation
 
@@ -32,6 +33,7 @@ class Api::UsersController < ApplicationController
       first_name: params[:first_name],
       last_name: params[:last_name],
       email: params[:email],
+      image: params[:image],
       password: params[:password],
       password_confirmation: params[:password_confirmation]
     )
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   has_secure_password
   has_many :posts
   has_many :comments, through: :posts
+  has_one_attached :image
 
 end
